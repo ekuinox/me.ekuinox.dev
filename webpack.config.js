4210e6dc0d007f3c8d80bcf798762e3b3aa2b81e
lm9 <depkey@me.com>
2019-04-05 12:00:42 +0900

    Reactに置き換え

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const env = process.env.WEBPACK_ENV == undefined ? 'development' : process.env.WEBPACK_ENV
+
+module.exports = {
+	mode: env,
+	devtool: env == 'development' ? "inline-source-map" : undefined,
+	entry: './src/Index.tsx',
+	output: {
+		path: `${__dirname}/dist`,
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	module: {
+		rules: [{
+			test: /\.tsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.css$/,
+			loader: ['style-loader', 'css-loader']
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "ekuinox's home page",
+			inject: false,
+			template: "./src/index.html"
+		})
+	]
+}
