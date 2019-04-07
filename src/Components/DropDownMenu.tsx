d8fccbab19ed6067a1983af2cb52f32782e55b53
lm9 <depkey@me.com>
2019-04-08 01:06:33 +0900

    menuのtext-decorationをnoneに

--- src/Components/DropDownMenu.tsx
+++ src/Components/DropDownMenu.tsx
@@ -2,8 +2,12 @@ import * as React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { Link } from 'react-router-dom'
+import { Link, LinkProps } from 'react-router-dom'
 import MenuIcon from '@material-ui/icons/Menu'
+
+const NoneTextDecorationLink: React.StatelessComponent<LinkProps> = (props) => {
+    return <Link to={props.to} style={{textDecoration: 'none'}}>{ props.children }</Link>
+}
 
 interface State {
     anchorEl: any
@@ -14,8 +18,18 @@ export default class DropDownMenu extends React.Component<{}, State> {
     constructor() {
         super({})
         this.state = { anchorEl: null }
+        this.onClick = this.onClick.bind(this)
+        this.onClose = this.onClose.bind(this)
     }
-    
+
+    onClick(event: React.MouseEvent<HTMLElement, MouseEvent>) {
+        this.setState({ anchorEl: event.currentTarget })
+    }
+
+    onClose() {
+        this.setState({ anchorEl: null })
+    }
+
     render() {
         const { anchorEl } = this.state;
 
@@ -24,7 +38,7 @@ export default class DropDownMenu extends React.Component<{}, State> {
                 <Button
                     aria-owns={anchorEl ? 'drop-down-menu' : undefined}
                     aria-haspopup="true"
-                    onClick={event => {this.setState({ anchorEl: event.currentTarget })}}
+                    onClick={this.onClick}
                 >
                     <MenuIcon />
                 </Button>
@@ -32,14 +46,26 @@ export default class DropDownMenu extends React.Component<{}, State> {
                     id="drop-down-menu"
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
-                    onClose={() => {this.setState({ anchorEl: null })}}
+                    onClose={this.onClose}
                 >
-                    <Link to="/"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Home</MenuItem></Link>
-                    <Link to="/contacts"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Contacts</MenuItem></Link>
-                    <Link to="/links"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Links</MenuItem></Link>
-                    <Link to="/products"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Products</MenuItem></Link>
-                    <Link to="/works"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Works</MenuItem></Link>
-                    <Link to="/likes"><MenuItem onClick={() => {this.setState({ anchorEl: null })}}>Likes</MenuItem></Link>
+                    <NoneTextDecorationLink to="/">
+                        <MenuItem onClick={this.onClose}>Home</MenuItem>
+                    </NoneTextDecorationLink>
+                    <NoneTextDecorationLink to="/contacts">
+                        <MenuItem onClick={this.onClose}>Contacts</MenuItem>
+                    </NoneTextDecorationLink>
+                    <NoneTextDecorationLink to="/links">
+                        <MenuItem onClick={this.onClose}>Links</MenuItem>
+                    </NoneTextDecorationLink>
+                    <NoneTextDecorationLink to="/products">
+                        <MenuItem onClick={this.onClose}>Products</MenuItem>
+                    </NoneTextDecorationLink>
+                    <NoneTextDecorationLink to="/works">
+                        <MenuItem onClick={this.onClose}>Works</MenuItem>
+                    </NoneTextDecorationLink>
+                    <NoneTextDecorationLink to="/likes">
+                        <MenuItem onClick={this.onClose}>Likes</MenuItem>
+                    </NoneTextDecorationLink>
                 </Menu>
             </div>
         );
