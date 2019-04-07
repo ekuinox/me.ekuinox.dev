132e7a28c5bf3b54e39905dc9461cf62faeb04a4
lm9 <depkey@me.com>
2019-04-08 04:11:53 +0900

    もうねむい

--- src/Components/DropDownMenu.tsx
+++ src/Components/DropDownMenu.tsx
@@ -10,6 +10,8 @@ import {path as LinksPath} from './Routes/Links'
 import {path as ProductsPath} from './Routes/Products'
 import {path as WorksPath} from './Routes/Works'
 import {path as LikesPath} from './Routes/Likes'
+import { ColorProperty } from 'csstype'
+import red from '@material-ui/core/colors/red'
 
 const NoneTextDecorationLink: React.StatelessComponent<LinkProps> = (props) => {
     return <Link to={props.to} style={{textDecoration: 'none'}}>{ props.children }</Link>
@@ -36,8 +38,30 @@ export default class DropDownMenu extends React.Component<{}, State> {
         this.setState({ anchorEl: null })
     }
 
+    
+
     render() {
         const { anchorEl } = this.state;
+        const MenuItemButton: React.StatelessComponent<{to: string, title: string, color?: ColorProperty}> = (props) => {
+            return (
+                <NoneTextDecorationLink to={props.to} >
+                    <MenuItem
+                        onClick={this.onClose}
+                        style={{
+                            height: '5vh',
+                            fontSize:'4vh',
+                        }}>
+                        <p style={{
+                            width: '80%',
+                            height: '100%',
+                            color: props.color,
+                            }}>
+                            {props.title}
+                        </p>
+                    </MenuItem>
+                </NoneTextDecorationLink>
+            )
+        }
 
         return (
             <div>
@@ -54,24 +78,12 @@ export default class DropDownMenu extends React.Component<{}, State> {
                     open={Boolean(anchorEl)}
                     onClose={this.onClose}
                 >
-                    <NoneTextDecorationLink to={AboutPath}>
-                        <MenuItem onClick={this.onClose}>Home</MenuItem>
-                    </NoneTextDecorationLink>
-                    <NoneTextDecorationLink to={ContactsPath}>
-                        <MenuItem onClick={this.onClose}>Contacts</MenuItem>
-                    </NoneTextDecorationLink>
-                    <NoneTextDecorationLink to={LinksPath}>
-                        <MenuItem onClick={this.onClose}>Links</MenuItem>
-                    </NoneTextDecorationLink>
-                    <NoneTextDecorationLink to={ProductsPath}>
-                        <MenuItem onClick={this.onClose}>Products</MenuItem>
-                    </NoneTextDecorationLink>
-                    <NoneTextDecorationLink to={WorksPath}>
-                        <MenuItem onClick={this.onClose}>Works</MenuItem>
-                    </NoneTextDecorationLink>
-                    <NoneTextDecorationLink to={LikesPath}>
-                        <MenuItem onClick={this.onClose}>Likes</MenuItem>
-                    </NoneTextDecorationLink>
+                    <MenuItemButton to={AboutPath} title="Home" color={red[300]} />
+                    <MenuItemButton to={ContactsPath} title="Contacts" color={red[300]} />
+                    <MenuItemButton to={LinksPath} title="Links" color={red[300]} />
+                    <MenuItemButton to={ProductsPath} title="Products" color={red[300]} />
+                    <MenuItemButton to={LikesPath} title="Likes" color={red[300]} />
+                    <MenuItemButton to={WorksPath} title="Works" color={red[300]} />
                 </Menu>
             </div>
         );
