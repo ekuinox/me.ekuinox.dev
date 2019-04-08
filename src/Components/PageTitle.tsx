af426bedb4ae673bb38b4f5a6dcb2a11329118b0
lm9 <depkey@me.com>
2019-04-08 13:49:57 +0900

    色

--- src/Components/PageTitle.tsx
+++ src/Components/PageTitle.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
-import { Typography } from '@material-ui/core';
-import { CSSProperties } from 'jss/css';
+import { Typography } from '@material-ui/core'
+import pink from '@material-ui/core/colors/pink'
+
 
 const PageTitle: React.StatelessComponent<{title: string, color?: string}> = (props) => (
     <Typography style={{
         fontSize: "4vh",
-        color: props.color
+        color: props.color ? props.color : pink[900]
     }}>{props.title}</Typography>
 )
 
