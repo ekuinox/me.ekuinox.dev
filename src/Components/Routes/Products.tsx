8833aa9ffedcaadba0a4357eeab6d970c2972c91
lm9 <depkey@me.com>
2019-04-23 23:53:45 +0900

    Qrunchで書いた記事を乗っけた

--- src/Components/Routes/Products.tsx
+++ src/Components/Routes/Products.tsx
@@ -8,6 +8,7 @@ export default () => (
 			<LI>
 				<a href="https://github.com/ekuinox/Fogo" target="_new">ekuinox/Fogo</a>
 				<br/>C++でゲームプログラム用にライブラリを自作しておるが、稚拙極まりないので良かったら優しく批評していただきたい。
+				<br/><a href="https://ekuinox.qrunch.io/entries/XpXMKec5us4tZIZT">ゲームプログラム向けのフレームワークを自作している話</a>をQrunchに投稿しました。
 			</LI>
 			<LI>
 				<a href="https://github.com/ekuinox/Khaos" target="_new">ekuinox/Khaos</a>
