9814017dc9216c1d9c76020ff9c08cb40b1275f0
ekuinox <depkey@me.com>
2019-09-05 16:53:06 +0900

    俺をバイトに雇ってくれ～～yarn start:dev!

--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { pink, grey } from '@material-ui/core/colors'
+import { Typography } from '@material-ui/core'
 import PageTitle from './PageTitle'
 
 const Page: React.StatelessComponent<{title: string, color?: string}> = (props) => (
@@ -14,10 +15,14 @@ const Page: React.StatelessComponent<{title: string, color?: string}> = (props) 
 		borderStyle: 'solid',
 		borderWidth: '2px',
 		borderColor: pink[200],
-		backgroundColor: grey[100]
+		backgroundColor: grey[100],
+		position: 'relative'
 	}}>
 		<PageTitle title={props.title} color={props.color} />
 		{props.children}
+		<a href='https://gist.github.com/ekuinox/4cda6cee3325412bcb18e9e53bdb5ce7' style={{textAlign: 'center', position: 'absolute', left: 0, right: 0, bottom: 0}}>
+			<Typography style={{fontSize: '5vh', marginLeft: '5vh', color: 'orange', fontStyle: 'oblique', fontFamily: 'ＭＳ Ｐゴシック', textDecorationLine: 'underline'}}>ekuinoxをアルバイトで雇って欲しい!!!!（詳細はここ）</Typography>
+		</a>
 	</div>
 )
 
