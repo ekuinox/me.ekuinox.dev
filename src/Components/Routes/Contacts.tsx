d88a436284df1570c6beb83bbed5d470019c6c99
ekuinox <depkey@me.com>
2019-11-06 00:39:20 +0900

    update twitter username

--- src/Components/Routes/Contacts.tsx
+++ src/Components/Routes/Contacts.tsx
@@ -5,7 +5,7 @@ import Page from '../Page'
 export default () => (
 	<Page title="Contacts">
 		<UL>
-			<LI><a href="https://twitter.com/ekuinox" target="_new">Twitter</a></LI>
+			<LI><a href="https://twitter.com/remokusu" target="_new">Twitter</a></LI>
 			<LI><a href="https://github.com/ekuinox" target="_new">GitHub</a></LI>
 			<LI><a href="https://mstdn.club/@lm0x" target="_new">Mastodon(mstdn.club)</a></LI>
 			<LI><a href="https://keybase.io/lm0x" target="_new">Keybase</a></LI>
