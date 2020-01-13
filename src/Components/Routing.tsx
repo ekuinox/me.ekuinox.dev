caae5af30e300202233d24482a4e9d812ce9317d
ekuinox <depkey@me.com>
2020-01-13 15:41:57 +0900

    #10 RoutingからProductsを消す

--- src/Components/Routing.tsx
+++ src/Components/Routing.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import About, {path as AboutPath} from './Routes/About'
 import Contacts, {path as ContactsPath} from './Routes/Contacts'
 import Links, {path as LinksPath} from './Routes/Links'
-import Products, {path as ProductsPath} from './Routes/Products'
 import Works, {path as WorksPath} from './Routes/Works'
 import Likes, {path as LikesPath} from './Routes/Likes'
 import NotFound from './Routes/NotFound'
@@ -15,7 +14,6 @@ const Routing: React.StatelessComponent<{}> = (props) => (
             <Route exact path={AboutPath} component={About} />
 			<Route path={ContactsPath} component={Contacts} />
 			<Route path={LinksPath} component={Links} />
-			<Route path={ProductsPath} component={Products} />
 			<Route path={WorksPath} component={Works} />
 			<Route path={LikesPath} component={Likes} />
 			<Route component={NotFound} />
