cee383ebe266e5d4ff0e6277d445d1cffbf7762e
ekuinox <depkey@me.com>
2020-01-13 16:23:24 +0900

    一回全部けしちゃえ

--- src/Components/Routes/About.tsx
+++ src/Components/Routes/About.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
-import { Typography } from '@material-ui/core'
 import Page from '../Page'
 
 export default () => (
-	<Page title="About" >
-		<Typography style={{fontSize: '3vh', marginLeft: '3vh'}}>lm0Xとかekuinoxとか名乗る．漫画と音楽が好き．</Typography>
+	<Page title="About">
 	</Page>
 )
 
