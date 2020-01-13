8c0e231f4177a00a400c2c30175f0f83b8fc9e49
ekuinox <depkey@me.com>
2020-01-13 17:08:03 +0900

    なんか長々と書いたけどちがうな～～

--- src/Components/Routes/About.tsx
+++ src/Components/Routes/About.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react'
 import Page from '../Page'
+import { Typography } from '@material-ui/core'
 
 export default () => (
 	<Page title="About">
+		<Typography
+			style={{
+				fontSize: '2vh',
+				margin: '3vh'
+			}}
+		>
+			<p>
+				れもくすと言います。ekuinoxやlm0Xなどと名乗ることもあります。
+			</p>
+			{ /*
+			<p>
+				プログラミングをやっています。言語はC++が主で、他にはRustやKotlin、Scalaなどを触っています（わからん）。<br />
+				言語以外で名前をあげるなら、UE4やReact(ここ)とかLaravelとかがわからんけどわからんなりにどうにかなります。
+			</p>
+			<p>
+				Progressive Houseが好きです。Future Houseも好き。Hard Danceも聴く、人物で言えばAiobahnがとても好き。RetroVisionも好き。
+			</p>
+			<p>
+				まんがタイムきらら好き。きんいろモザイクのアニメ放映からMAXを追うようになって、どうして私が美術科に!?をゲストから追っていた。良かった。浦上紫苑さんが好き。<br />
+				きららだと他には海色マーチとか球詠とかが好き。連載終了しているもので言えば、ぱぺっとコール！とかセカイ魔王が好き。私のGitHubのアイコンはぱぺっとコール！のつるちゃんです。<br />
+			</p>
+			*/ }
+		</Typography>
 	</Page>
 )
 
