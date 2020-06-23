0495364a3fa29ec689f04c4415695f72d4f9862c
ekuinox <depkey@me.com>
2020-06-23 21:26:53 +0900

    rename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/global.css';
+import '../styles/global.scss';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <Component {...pageProps} />
