269ed1267bc249cab1763704d6620ea2c150c680
ekuinox <depkey@me.com>
2020-06-28 11:44:40 +0900

    f-mでアコーディオン作った

---
+++ src/components/about/accordion.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+/**
+ * https://codesandbox.io/s/framer-motion-accordion-qx958?file=/src/Example.tsx
+ */
+export const Accordion = (props: {
+  children: ReactNode;
+  expanded: boolean;
+}): JSX.Element => (
+  <AnimatePresence initial={false}>
+    {props.expanded && (
+      <motion.section
+        key="content"
+        initial="collapsed"
+        animate="open"
+        exit="collapsed"
+        variants={{
+          open: { opacity: 1, height: 'auto' },
+          collapsed: { opacity: 0, height: 0 },
+        }}
+        transition={{ duration: 0.4 }}
+      >
+        {props.children}
+      </motion.section>
+    )}
+  </AnimatePresence>
+);
