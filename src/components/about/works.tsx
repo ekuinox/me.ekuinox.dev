b62da636dff48d1f75cba24b0169596beaa35334
ekuinox <depkey@me.com>
2020-06-28 11:45:17 +0900

    worksから切り分けをやったがめちゃくちゃにダサい

--- src/components/about/works.tsx
+++ src/components/about/works.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { Accordion } from './accordion';
+import { Label } from './label';
 
 /**
  * 成果物のUrlとタイトルのペアのリスト
@@ -29,40 +31,14 @@ const Anchor = styled.a`
   font-size: 2vh;
 `;
 
-const Label = styled.label`
-  font-size: 15px;
-`;
-
-const Accordion = styled(
-  ({
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    isOpened,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    durations,
-    ...props
-  }: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  > & {
-    isOpened: boolean;
-    durations?: { [key in 'open' | 'close']: number };
-  }) => <div {...props} />
-)`
-  max-height: ${(props) => (props.isOpened ? '100%' : '0')};
-  overflow: hidden;
-  transition: max-height;
-  ${(props) =>
-    props.isOpened
-      ? props.durations?.close ?? css`10ms`
-      : props.durations?.open ?? css`10ms`};
-`;
-
 export const Works = (): JSX.Element => {
-  const [isOpened, setOpened] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   return (
     <>
-      <Label onClick={() => setOpened(!isOpened)}>{'私の制作物'}</Label>
-      <Accordion isOpened={isOpened}>
+      <Label onClick={() => setExpanded(!expanded)} isActive={expanded}>
+        {'私の制作物'}
+      </Label>
+      <Accordion expanded={expanded}>
         <Ul>
           {deliverables.map(([url, title]) => (
             <Li key={url}>
