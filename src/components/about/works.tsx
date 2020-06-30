e0de3182ef395157852e07186242e5e27c1bd753
ekuinox <depkey@me.com>
2020-06-30 22:37:09 +0900

    プチ紹介をいれるように

--- src/components/about/works.tsx
+++ src/components/about/works.tsx
@@ -6,10 +6,23 @@ import { Label } from './label';
 /**
  * 成果物のUrlとタイトルのペアのリスト
  */
-const deliverables: [string, string][] = [
-  ['https://github.com/ekuinox/Fogo', 'ekuinox/Fogo'],
-  ['https://github.com/mcymze/Khaos', 'mcymze/Khaos'],
-  ['https://github.com/ekuinox/red_drink', 'ekuinox/red_drink'],
+const deliverables: [string, string, string][] = [
+  ['https://github.com/ekuinox/Fogo', 'Fogo', 'C++で実装したECC的なライブラリ'],
+  [
+    'https://github.com/mcymze/Khaos',
+    'Khaos',
+    'Kotilinで実装したSpigot向けのDigAll',
+  ],
+  [
+    'https://github.com/mcymze/IntegrativeYmzeServerPlugin',
+    'IntegrativeYmzeServerPlugin',
+    'Scalaで実装しているSpigotのプラグイン',
+  ],
+  [
+    'https://github.com/ekuinox/red_drink',
+    'red_drink',
+    'Rustで実装している途中の自宅サーバのダッシュボード',
+  ],
 ];
 
 const Ul = styled.ul`
@@ -25,25 +38,39 @@ const Li = styled.li`
 `;
 
 const Anchor = styled.a`
-  font-size: 2vh;
+  font-size: 18px;
+  text-decoration: underline;
+`;
+
+const Paragraph = styled.p`
+  font-size: 15px;
+  padding: 1vh 0 0 0;
+`;
+
+const Box = styled.div`
+  padding: 1vh 1vw;
 `;
 
 export const Works = (): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <>
+    <Box>
       <Label onClick={() => setExpanded(!expanded)} isActive={expanded}>
         {'works'}
       </Label>
       <Accordion expanded={expanded}>
         <Ul>
-          {deliverables.map(([url, title]) => (
+          {deliverables.map(([url, title, desc]) => (
             <Li key={url}>
-              <Anchor href={url}>{title}</Anchor>
+              <div>
+                <Anchor href={url}>{title}</Anchor>
+                <Paragraph>{desc}</Paragraph>
+              </div>
             </Li>
           ))}
         </Ul>
+        <hr />
       </Accordion>
-    </>
+    </Box>
   );
 };
