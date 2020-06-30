a8b1471da18784f67866b1ceda84a057563c2697
ekuinox <depkey@me.com>
2020-06-30 22:44:15 +0900

    y軸で100vh以上でも背景を塗りつぶすように

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  overflow-y: auto;
   margin: 0;
   padding: 0;
   background-color: #f0ffff;
