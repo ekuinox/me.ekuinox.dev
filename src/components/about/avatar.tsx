3626dad7f1208a1ca4db466abe9819e3eae4e178
ekuinox <depkey@me.com>
2020-06-30 23:29:08 +0900

    avatarの読み込みが遅くて、先にボーダだけが変な感じに描画されちゃうんだけど、これで解決しねえかな

--- src/components/about/avatar.tsx
+++ src/components/about/avatar.tsx
@@ -6,7 +6,12 @@ export const Avatar = styled(
   (props: { src: string; alt?: string }): JSX.Element => (
     <motion.img
       initial={{ scale: 0.8 }}
-      animate={{ scale: 1 }}
+      animate={{
+        scale: 1,
+        borderColor: '#f26f9d',
+        borderWidth: '2px',
+        borderStyle: 'solid',
+      }}
       whileTap={{ scale: 0.9 }}
       {...props}
     />
@@ -14,8 +19,5 @@ export const Avatar = styled(
 )`
   height: 150px;
   border-radius: 50%;
-  border-style: solid;
-  border-width: 2px;
-  border-color: #f26f9d;
   margin: 1vh 0;
 `;
