20a67ea928371bc87fe90605d95f3bf96a996221
ekuinox <depkey@me.com>
2020-07-01 23:22:26 +0900

    rename

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -13,7 +13,7 @@ const Hr = styled.hr`
   padding: 1vh 0;
 `;
 
-const Paragraph = styled.p`
+const Bio = styled.p`
   margin: auto;
   word-wrap: break-word;
   padding: 1vh 0;
@@ -24,10 +24,10 @@ export const About = (): JSX.Element => (
     <Avatar src={'/assets/avatar.jpg'} />
     <H1>{'ekuinox | れもくす'}</H1>
     <Social />
-    <Paragraph>{`
+    <Bio>{`
 			こんにちは。関西でプログラマをやることで生きているオタクです。
       漫画と音楽が好きです。どうして私が美術科に!?とAiobahnを推しています。
-    `}</Paragraph>
+    `}</Bio>
     <Hr />
     <Works />
   </>
