72ca3e2d29339341eff498069410f7d934b7fec8
ekuinox <depkey@me.com>
2020-07-01 23:16:28 +0900

    Socialに置き換え

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Icon } from '@iconify/react';
-import TwitterIcon from '@iconify/icons-ant-design/twitter-outlined';
-import GitHubIcon from '@iconify/icons-ant-design/github-outlined';
-import EmailIcon from '@iconify/icons-ant-design/mail-outlined';
-import SteamIcon from '@iconify/icons-cib/steam';
+import { Social } from './social';
 import { Works } from './works';
 import { Avatar } from './avatar';
 
@@ -23,45 +19,11 @@ const Paragraph = styled.p`
   padding: 1vh 0;
 `;
 
-const Social = {
-  Ul: styled.ul`
-    display: table;
-    border-collapse: separate;
-    border-spacing: 2px 0;
-    vertical-align: middle;
-    margin: 0 auto;
-    padding: 1vh 0;
-  `,
-  Li: styled.li`
-    display: table-cell;
-    vertical-align: middle;
-    text-align: center;
-    padding: 0 3vw;
-  `,
-  Anchor: styled.a`
-    display: block;
-    font-size: 30px;
-  `,
-};
-
 export const About = (): JSX.Element => (
   <>
     <Avatar src={'/assets/avatar.jpg'} />
     <H1>{'ekuinox | れもくす'}</H1>
-    <Social.Ul>
-      {[
-        { url: 'https://twitter.com/remokusu', icon: TwitterIcon },
-        { url: 'https://github.com/ekuinox', icon: GitHubIcon },
-        { url: 'https://steamcommunity.com/id/lm0x', icon: SteamIcon },
-        { url: 'mailto:depkey@me.com', icon: EmailIcon },
-      ].map(({ url, icon }) => (
-        <Social.Li key={url}>
-          <Social.Anchor href={url} target="_blank" rel="noreferrer noopener">
-            <Icon icon={icon} />
-          </Social.Anchor>
-        </Social.Li>
-      ))}
-    </Social.Ul>
+    <Social />
     <Paragraph>{`
 			こんにちは。関西でプログラマをやることで生きているオタクです。
       漫画と音楽が好きです。どうして私が美術科に!?とAiobahnを推しています。
