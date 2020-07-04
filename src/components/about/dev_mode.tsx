07a47819eab6ef374d02f0d22afa3c43982a0770
ekuinox <depkey@me.com>
2020-07-05 03:23:36 +0900

    add media query

--- src/components/about/dev_mode.tsx
+++ src/components/about/dev_mode.tsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { SnackBar } from './snackbar';
+import media from 'styled-media-query';
+
+const Message = styled.div`
+  background-color: #65fcd9;
+  color: #003327;
+  text-align: center;
+  ${media.lessThan('medium')`
+    padding: 5px;
+    width: 95vw;
+    border-radius: 5px;
+  `}
+  ${media.greaterThan('medium')`
+    padding: 5px 15px;
+    border-radius: 10px / 15px;
+	`}
+`;
 
 export const DevMode = ({ count }: { count: number }): JSX.Element => {
-  const [message, setMessage] = useState('');
-  const [enabled, setEnabled] = useState(false);
+  const [message, setMessage] = useState('デベロッパーになるにはあと4回タップしてね');
+  const [enabled, setEnabled] = useState(true);
   useEffect(() => {
     if (count === 3) {
       setEnabled(true);
@@ -18,8 +35,12 @@ export const DevMode = ({ count }: { count: number }): JSX.Element => {
   }, [count]);
   useEffect(() => {
     if (enabled) {
-      setTimeout(() => setEnabled(false), 5000);
+      //setTimeout(() => setEnabled(false), 5000);
     }
   });
-  return <SnackBar enabled={enabled}>{message}</SnackBar>;
+  return (
+    <SnackBar enabled={enabled}>
+      <Message>{message}</Message>
+    </SnackBar>
+  );
 };
