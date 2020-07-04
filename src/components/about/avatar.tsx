09c39628d7e64e42619a76c1ff2d8ecda2290182
ekuinox <depkey@me.com>
2020-07-05 02:49:51 +0900

    add onClick

--- src/components/about/avatar.tsx
+++ src/components/about/avatar.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const Avatar = styled(
-  (props: { src: string; alt?: string }): JSX.Element => (
+  (props: { src: string; alt?: string; onClick?: () => void }): JSX.Element => (
     <motion.img
       initial={{ scale: 0.8 }}
       animate={{
