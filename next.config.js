9a76287bd35fb241ce7d1ecc0175246f2fd280f0
ekuinox <depkey@me.com>
2020-12-10 21:31:29 +0900

    MDXが使えるように

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withMDX = require('@next/mdx')();
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
 
 module.exports = withMDX({
   target: 'serverless',
+  pageExtensions: ['tsx', 'mdx'],
 });
